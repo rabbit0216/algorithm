4992c7747c122c4fb945450bd7ac686525d9bb15
better <67730991+rabbit0216@users.noreply.github.com>
2023-02-06 21:48:40 +0900

    [Silver II] Title: 유기농 배추, Time: 120 ms, Memory: 13812 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+    public static int R, C, K;
+    public static int[][] graph;
+    public static int[][] dist;
+    public static boolean[][] visited;
+    public static Queue<Node> adj;
+    public static Queue<Node> all;
+    public static int ans;
+
+    public static class Node {
+        int r, c;
+
+        Node(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        for (int test_case = 0; test_case < T; test_case++) {
+            st = new StringTokenizer(br.readLine());
+            C = Integer.parseInt(st.nextToken());
+            R = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            graph = new int[R][C];
+            visited = new boolean[R][C];
+            ans = 0;
+            adj = new LinkedList<>();
+            all = new LinkedList<>();
+            for (int k = 0; k < K; k++) {
+                st = new StringTokenizer(br.readLine());
+                int c = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+                graph[r][c] = 1;
+                all.offer(new Node(r, c));
+            }
+            bfs();
+            System.out.println(ans);
+        }
+    }
+
+    public static void bfs() {
+        int[][] move = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        while (!all.isEmpty()) {
+            Node cur = all.poll();
+            if(visited[cur.r][cur.c]) continue;
+            while (true) {
+                for (int dir = 0; dir < move.length; dir++) {
+                    int nr = cur.r + move[dir][1];
+                    int nc = cur.c + move[dir][0];
+
+                    if (!scope(nr, nc)) continue;
+                    if (visited[nr][nc]) continue;
+                    if (graph[nr][nc] == 1) {
+                        adj.offer(new Node(nr, nc));
+                        visited[nr][nc] = true;
+                    }
+                }
+                if (adj.isEmpty()) {
+                    break;
+                }
+                cur = adj.poll();
+            }
+            ans++;
+        }
+    }
+
+    public static boolean scope(int r, int c) {
+        return r >= 0 && r < R && c >= 0 && c < C;
+    }
+}
