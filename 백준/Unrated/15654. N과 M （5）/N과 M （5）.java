1eb7560af846fc13bc546fe7a3c7c744f744d432
better <67730991+rabbit0216@users.noreply.github.com>
2023-04-09 06:46:37 +0900

    [Unrated] Title: N과 M (5), Time: 220 ms, Memory: 30588 KB -BaekjoonHub

---
+++ 백준/Unrated/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N,M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        makePerm(0, new int[M], new boolean[N]);
+        System.out.println(sb);
+    }
+
+    static void makePerm(int nthChoice, int[] choice, boolean[] visited){
+        if(nthChoice == choice.length){
+            for (int i = 0; i < choice.length; i++) {
+                sb.append(choice[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if(!visited[i]){
+                visited[i] = true;
+                choice[nthChoice] = arr[i];
+                makePerm(nthChoice+1, choice, visited);
+                visited[i] = false;
+            }
+        }
+    }
+}
