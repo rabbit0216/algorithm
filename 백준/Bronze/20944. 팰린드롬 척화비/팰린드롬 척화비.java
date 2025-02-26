d72524ee31acf3ee352f559ef11788f0fa461861
better <67730991+rabbit0216@users.noreply.github.com>
2025-02-26 18:52:57 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 112 ms, Memory: 19172 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append('a');
+        }
+
+        bw.write(sb.toString()); bw.flush(); bw.close();
+    }
+}
